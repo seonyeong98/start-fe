d8dc897500052c977f7a4a93ba4f3e867dc2e352
seonyeong98 <seon02_23@naver.com>
2021-03-17 17:58:01 +0900

    01 수업 과제
    변수, 함수, 데이터 타입, object, 배열 등에 대해 배워보았다.

---
+++ 01/app.js
@@ -0,0 +1,86 @@
+console.log('app.js');
+
+// var name = '박선영';
+// var Name = '박선영';
+
+// console.log(name);
+// console.log(Name);
+//자바스크립트는 이름이 같아도 에러 발생하지 않음
+
+/*var var ='test';
+console.log(var)
+var는 변수로 선언할 수 없음*/
+
+//데이터 타입
+
+// var count =5;
+// var String = '박선영';
+
+// console.log(count, String);
+
+// var isMan = true;  //bool
+// var isMan ="true"; //String
+
+// console.log(isMan);
+
+// //function
+
+// function log(str) {
+//     console.log(str);
+// }
+
+// log('hello');
+
+// var log = function(str);
+
+//object
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+// };
+
+// var image1 = [1, 2, 3, 4, 'a']; //어떤 데이터 타입이든 들어갈 수 있지만 통일해 주는 것이 오류를 줄일 수 있음
+// image1[0] ='a';
+
+// console.log(image, image1);
+
+// var data = null;
+// var data ='';
+// var data2; //아무것도 선언하지 않으면 undifined
+
+// console.log(data, data1, data2);
+// //object data type의 장점은 하나로 묶어 준다는 것
+
+// console.log(1+2);
+// console.log('a' + 'b');
+// console.log(1 +'4');
+
+// var count = 1;
+// var number ='2';
+
+// console.log(count+number);
+//자바스크립트는 형변환이 자유로워 데이터 타입이 달라도 합쳐버림
+
+// console.log(1 == 1); //true
+// console.log(1 == 2);//false
+
+// console.log(0 == ''); //true 제대로 검사하려면 === 3개 사용
+
+// if(true){
+//     console.log(1);
+// }
+
+// var isHero = true;
+
+// if(!isHero) {
+// console.log('Hero');
+// }
+
+// console.log(typeof 1);
+// console.log(typeof[]);
+// console.log(typeof'');
+
+
+
