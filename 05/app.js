bb10473197b863de8b20937383d6f4a300baa587
seonyeong98 <seon02_23@naver.com>
2021-04-15 16:59:52 +0900

    05수업

---
+++ 05/app.js
@@ -0,0 +1,123 @@
+// let a = 'aa';
+// const b = 'bb';
+// var c = '11';
+// c = 33;
+// b = 11;
+
+// console.log(a, b, c);
+
+// var str = 'aaaa \
+// fasdf';
+// const s = `aaaa
+// bbb
+// ccc`;
+// console.log(str, s);
+
+// function sum(x,y =1) {
+//     return x+y;
+// }
+
+// console.log(sum(1));
+
+//화살표 함수
+
+//예전방법
+// const arr = [1, 2, 3];
+
+// const newArr = arr.map(function(item) {
+
+//     return item +1;
+// });
+//  console.log(newArr);
+
+// const arr = [1, 2, 3];
+
+// const newArr = arr.map((item) => {
+
+//     return item + 1;
+// });
+//  console.log(newArr);
+
+// const arr = [1, 2, 3];
+
+// const newArr = arr.map((item) =>item +1);
+// console.log(newArr);
+
+// document
+// .getElementById('box')
+// .addEventListener('click', (event) => console.log(1));
+
+// //전개구문...
+// function sum(x, y, z) {
+//     return x+ y+ z;
+// }
+// const numbers = [1, 2, 3];
+// console.log(sum(...numbers)); //1+2+3 ->6
+
+//Rest 파라미터
+// function func(...param) {
+//     console.log(param);
+// }
+
+// const number = 1234;
+
+// const student ={
+//     number
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
